5b705256d82cb4db17a5576bdc2bbcf5b5daad7c
nguyennn15 <nguyen151205@gmail.com>
2026-04-08 18:14:28 +0700

    Thêm tính năng Đăng Xuất vào settings

--- ChatApp/app/src/main/java/com/example/chatapp/view/setting/SettingsActivity.java
+++ ChatApp/app/src/main/java/com/example/chatapp/view/setting/SettingsActivity.java
@@ -5,12 +5,14 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.Switch;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 
 import com.example.chatapp.R;
 import com.example.chatapp.view.chat.ChatListActivity;
 import com.example.chatapp.view.darkmode.BaseActivity;
+import com.example.chatapp.view.login.LoginActivity;
 
 public class SettingsActivity extends BaseActivity {
     private static final String PREF_NAME = "ChatAppPrefs";
@@ -46,5 +48,28 @@ public class SettingsActivity extends BaseActivity {
             AppCompatDelegate.setDefaultNightMode(isChecked ?
                     AppCompatDelegate.MODE_NIGHT_YES : AppCompatDelegate.MODE_NIGHT_NO);
         });
+        findViewById(R.id.btnLogout).setOnClickListener(v -> showLogoutDialog());
+    }
+
+    private void showLogoutDialog() {
+        new AlertDialog.Builder(this, R.style.AlertDialogTheme)
+                .setTitle("Đăng xuất")
+                .setMessage("Bạn có chắc muốn đăng xuất khỏi tài khoản này không?")
+                .setPositiveButton("Đăng xuất", (dialog, which) -> performLogout())
+                .setNegativeButton("Hủy", null)
+                .show();
+    }
+
+    private void performLogout() {
+        SharedPreferences prefs = getSharedPreferences(PREF_NAME, MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.remove("myUserId");
+        // editor.remove(KEY_DARK_MODE);
+        editor.apply();
+
+        Intent intent = new Intent(SettingsActivity.this, LoginActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+        finish();
     }
 }
